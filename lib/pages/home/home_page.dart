54d6afa45fb96626b73b827e2944fc8796131613
unknown <manhhoangg2k2@gmail.com>
2024-03-14 10:50:27 +0700

    first commit

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomePage'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('change page category'),
+          onPressed: () {
+          },
+        ),
+      ),
+    );
+  }
+}
